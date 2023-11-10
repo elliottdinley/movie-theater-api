06e5ebf2ec5f96fe4c2e9a47c579f2b4124872f2
elliottdinley <elliott.dinley1@gmail.com>
2023-11-10 11:35:45 +0000

    server-side validation

--- src/routes/shows.js
+++ src/routes/shows.js
@@ -4,52 +4,126 @@ const router = express.Router();
 const { Show, User } = require('../models/index');
 
 router.get('/', async (request, response) => {
-    const data = await Show.findAll();
-    response.json(data);
+    try {
+        const data = await Show.findAll();
+        response.json(data);
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 router.get('/:id', async (request, response) => {
     const showId = request.params.id;
-    const data = await Show.findByPk(showId);
-    response.json(data);
+
+    try {
+        const data = await Show.findByPk(showId);
+
+        if (data) {
+            response.json(data);
+        } else {
+            response.status(404).json({ error: 'Show not found' });
+        }
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 router.get('/genre/:genre', async (request, response) => {
     const genre = request.params.genre;
-    const data = await Show.findAll({
-        where: { genre: genre }
-    })
-    response.json(data);
+
+    try {
+        const data = await Show.findAll({
+            where: { genre: genre }
+        })
+        response.json(data);
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 router.put('/:id/update-rating', async (request, response) => {
     const showId = request.params.id;
     const newRating = request.body.rating;
-    const show = await Show.findByPk(showId);
-    await Show.update(
-        { rating: newRating },
-        { where: { id: showId } }
-    )
-    response.json({ message: 'Success!' });
+
+    try {
+        if (isNan(newRating) || newRating < 0 || newRating > 5) {
+            response.status(400).json({ error: 'Invalid rating value' });
+        } else {
+            const show = await Show.findByPk(showId);
+            if (show) {
+                await Show.update(
+                    { rating: newRating },
+                    { where: { id: showId } }
+                )
+                response.json({ message: 'Success!' });
+            } else {
+                response.status(404).json({ error: 'Show not found' });
+            }
+        }
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 router.put('/:id/update-status', async (request, response) => {
     const showId = request.params.id;
     const newStatus = request.body.available;
-    const show = await Show.findByPk(showId);
-    await Show.update(
-        { available: newStatus },
-        { where: { id: showId } }
-    )
-    response.json({ message: 'Success!' });
+
+    try {
+        if (typeof newStatus === 'boolean') {
+            const show = await Show.findByPk(showId);
+            if (show) {
+                await Show.update(
+                    { available: newStatus },
+                    { where: { id: showId } }
+                )
+                response.json({ message: 'Success!' });
+            } else {
+                response.status(404).json({ error: 'Show not found' });
+            }
+        } else {
+            response.status(400).json({ error: 'Invalid available status value' });
+        }
+
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 router.delete('/:id', async (request, response) => {
     const showId = request.params.id;
-    await Show.destroy({
-        where: { id: showId }
-    });
-    response.json({ message: 'Success!' });
+
+    try {
+        const numDeleted = await Show.destroy({
+            where: { id: showId }
+        });
+
+        if (numDeleted) {
+            response.json({ message: 'Success!' });
+        } else {
+            response.status(404).json({ error: 'Show not found' });
+        }
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,33 +4,82 @@ const router = express.Router();
 const { Show, User } = require('../models/index');
 
 router.get('/', async (request, response) => {
-    const data = await User.findAll()
-    response.json(data);
+    try {
+        const data = await User.findAll()
+        response.json(data);
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 router.get('/:id', async (request, response) => {
     const userId = request.params.id;
-    const data = await User.findByPk(userId);
-    response.json(data);
+
+    try {
+        const data = await User.findByPk(userId);
+
+        if (data) {
+            response.json(data);
+        } else {
+            response.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 router.get('/:id/shows', async (request, response) => {
     const userId = request.params.id;
-    const data = await User.findByPk(userId, {
-        include: [{ model: Show, through: 'watched' }]
-    });
-    response.json(data);
+
+    try {
+        const data = await User.findByPk(userId, {
+            include: [{ model: Show, through: 'watched' }]
+        });
+        if (data) {
+            response.json(data);
+        } else {
+            response.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
+
 });
 
 router.put('/:userId/add-show/:showId', async (request, response) => {
     const userId = request.params.userId;
     const showId = request.params.showId;
 
-    const user = await User.findByPk(userId);
-    const show = await Show.findByPk(showId);
+    try {
+        const user = await User.findByPk(userId);
+        const show = await Show.findByPk(showId);
 
-    await user.addShow(show);
-    response.json({ message: 'Success!' });
+        if (user && show) {
+            const hasWatched = await user.hasShow(show);
+            if (!hasWatched) {
+                await user.addShow(show);
+                response.json({ message: 'Success!' });
+            } else {
+                response.status(400).json({ error: 'User has already watched this show' });
+            }
+        } else {
+            return response.status(404).json({ error: 'User or show not found' });
+        }
+    } catch (error) {
+        response.status(500).json({
+            error: 'Internal Server Error',
+            details: error
+        });
+    }
 });
 
 module.exports = router;
